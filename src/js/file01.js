57dd36a737278acf5faa3b53eafadf3c8eac90ec
Christian777-Ing <cezavala@espol.edu.ec>
2026-06-08 19:21:56 +0000

    Cambios Clase 08/06

---
+++ src/js/file01.js
@@ -0,0 +1,23 @@
+"use strict";
+ 
+const showToast = () => {
+    const toast = document.getElementById("toast-interactive");
+    if (toast) {
+        toast.classList.add("md:block");
+    }
+};
+const showVideo = () => {
+    const demo = document.getElementById("Promociones_b");
+    if (demo) {
+        demo.addEventListener("click", () => {
+            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+        });
+    }
+};
+
+(() => {
+    showToast();
+    showVideo();
+})();
+
+
