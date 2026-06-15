67a5bc8f27fad1b2932cb7691c5f1ba6b633363b
Christian777-Ing <cezavala@espol.edu.ec>
2026-06-15 19:47:24 +0000

    clase 15/06

--- src/js/file01.js
+++ src/js/file01.js
@@ -1,8 +1,43 @@
 "use strict";
 import { fetchProducts } from "./functions.js";
+import { fetchCategories } from "./functions.js";
+
+const renderCategories = async () => {
+    try {
+        const result = await fetchCategories(
+            'https://data-dawm.github.io/datum/reseller/categories.xml'
+        );
+
+        if (result.success) {
+            const container = document.getElementById("categories");
+
+            container.innerHTML = `
+                <option value="" disabled selected>
+                    Seleccione una categoría
+                </option>
+            `;
+            const categoriesXML = result.body;
+            const categories = categoriesXML.getElementsByTagName("category");
+            for (let category of categories) {
+                let categoryHTML = `<option value="[ID]">[NAME]</option>`;
+                const id = category.getElementsByTagName("id")[0].textContent;
+                const name = category.getElementsByTagName("name")[0].textContent;
+
+                categoryHTML = categoryHTML.replace("[ID]", id);
+                categoryHTML = categoryHTML.replace("[NAME]", name);
+
+                container.innerHTML += categoryHTML;
+            }
+        } else {
+            alert(result.message);
+        }
+    } catch (error) {
+        alert(error.message || error);
+    }
+};
 
 let renderProducts = () => {
-    fetchProducts('https://data-dawm.github.io/datum/reseller/products.json')
+    fetchProducts('https://gist.githubusercontent.com/Christian777-Ing/d7c3ff807b067a73e2f0fd5dba99820d/raw/7fee14248496f2964d8a10c05d7182e747463d63/menu.json')
         .then(result => {
             if (result.success) {
                 let container = document.getElementById("products-container");
@@ -23,17 +58,17 @@ let renderProducts = () => {
        <div class="h-5 rounded w-full">[PRODUCT.TITLE]</div>
            <div class="space-y-2">
                <a href="[PRODUCT.PRODUCTURL]" target="_blank" rel="noopener noreferrer"
-               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full inline-block">
-                   Ver en Amazon
+               class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full inline-block">
+                   Ordenar Ahora
                </a>
                <div class="hidden"><span class="1">[PRODUCT.CATEGORY_ID]</span></div>
            </div>
        </div>
    </div>`;
 
-                    productHTML = productHTML.replaceAll("[PRODUCT.TITLE]", product.title);
+                    productHTML = productHTML.replaceAll("[PRODUCT.TITLE]", product.title.length > 20 ? product.title.substring(0, 20) + "..." : product.title);
                     productHTML = productHTML.replaceAll("[PRODUCT.IMGURL]", product.imgUrl);
-                    productHTML= productHTML.replaceAll("[PRODUCT.PRICE]", product.price);
+                    productHTML = productHTML.replaceAll("[PRODUCT.PRICE]", product.price);
                     container.innerHTML += productHTML;
                 })
             }
@@ -57,6 +92,7 @@ const showVideo = () => {
 
 (() => {
     renderProducts();
+    renderCategories();
     showToast();
     showVideo();
 })();
